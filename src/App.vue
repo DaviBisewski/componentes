8b5abf2f72ec1d4bc7b0cd20bb31b9456f3dea8e
DaviBisewski <davimbisewski@gmail.com>
2024-07-12 15:10:33 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import ExpandBox from './components/ExpandBox.vue';
+</script>
+
+<template>
+  <h1>Componentes</h1>
+  <hr>
+
+  <ExpandBox title="Caixa 1"
+  content="Essa é a LOUD" 
+  />
+  <expand-box title="Caixa 2" 
+  content="Então Isqueci"
+  />
+
+</template>
+
+<style scoped></style>
---
+++ src/components/ExpandBox.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref, computed } from 'vue';
+const props = defineProps(['title', 'content'])
+const ShowBox = ref(false)
+const textobotao = computed(() => ShowBox.value ? 'Esconder' : 'Mostrar')
+</script>
+
+<template>
+    <div>
+        <button @click="ShowBox =! ShowBox">{{ textobotao }}</button>
+        <div v-if="ShowBox" class="expand-box">
+            <h1>{{ props.title }}</h1>
+            <p>{{ props.content }}</p>
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+.expand-box {
+
+    width: 300px;
+    height: 100%;
+    padding: 20px;
+    border: 1px solid black;
+    box-shadow: 2px 2px 2px 2px gray;
+}
+</style>
